0864c14c7f198e3e6ca447ec6ab2dc90e8090096
Ahmed Arafa <ahmed.arafa.fawzy@gmail.com>
2025-02-23 20:01:13 +0200

    Session 06 Assignment

--- Session 06/LeetCode.dart
+++ Session 06/LeetCode.dart
@@ -1,22 +1,24 @@
 /*
 LeetCode Question
 
-217. Contains Duplicate
-Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
 
 Example 1:
-Input: nums = [1,2,3,1]
-Output      : true
-Explanation : The element 1 occurs at the indices 0 and 3.
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 Example 2:
-Input: nums = [1,2,3,4]
-Output      :  false
-Explanation :  All elements are distinct.
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
 
 Example 3:
-Input: nums = [1,1,1,3,3,4,3,2,4,2]
-Output: true
+Input: nums = [3,3], target = 6
+Output: [0,1]
 */
 
 void main() {
